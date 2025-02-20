fa9f3a4e13916ebfc8233e87fc966f0330aacbbc
theclassworks <2300@mail.ir>
2025-02-20 08:39:56 +0330

    compidan

---
+++ Group1/app1/assets/js/main.js
@@ -0,0 +1,9 @@
+let user1 = { fname: "علی", age: 30 };
+let user2 = { fname: "میترا", age: 15 };
+
+let users = [user1, user2];
+
+console.log(user1["fname"]);
+console.log(user2["age"]);
+
+console.log(users[0]["age"]);
